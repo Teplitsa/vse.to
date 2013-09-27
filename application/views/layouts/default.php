7a1f005c2f173919ea05ed1ae8b5c7ca51469134
Alexey Romanov <jgc128@outlook.com>
2013-09-27 16:00:03 +0400

    Add Reformal

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -64,5 +64,28 @@
     jQuery(function($){
         $('.help-pop').tooltip()
     });
-</script>                            
+</script>    
+
+<!-- Reformal -->
+<script type="text/javascript">
+    var reformalOptions = {
+        project_id: 184340,
+        project_host: "vseto.reformal.ru",
+        tab_orientation: "left",
+        tab_indent: "50%",
+        tab_bg_color: "#333333",
+        tab_border_color: "#FFFFFF",
+        tab_image_url: "http://tab.reformal.ru/T9GC0LfRi9Cy0Ysg0Lgg0L%252FRgNC10LTQu9C%252B0LbQtdC90LjRjw==/FFFFFF/2a94cfe6511106e7a48d0af3904e3090/left/1/tab.png",
+        tab_border_width: 0
+    };
+    
+    (function() {
+        var script = document.createElement('script');
+        script.type = 'text/javascript'; script.async = true;
+        script.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'media.reformal.ru/widgets/v3/reformal.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+    })();
+</script><noscript><a href="http://reformal.ru"><img src="http://media.reformal.ru/reformal.png" /></a><a href="http://vseto.reformal.ru">Oтзывы и предложения для vse.to</a></noscript>
+
+                        
 </body>     
