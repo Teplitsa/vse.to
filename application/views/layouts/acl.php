0d77cc4c4cbc1eed023c6402cab237e871b01c04
Alexey Romanov <jgc128@outlook.com>
2013-10-11 12:40:42 +0400

    Reformal & sidebar header

--- application/views/layouts/acl.php
+++ application/views/layouts/acl.php
@@ -30,7 +30,7 @@
                 </div>
 				<aside class="span4">
 					<div class="right-block">
-						<h2>О проекте</h2>
+						<h2>Что такое vsё.to</h2>
 						<div class="right-block-content">
                                                     <?php echo Widget::render_widget('blocks', 'block', 'short_about'); ?>
 						</div>
@@ -60,5 +60,26 @@
 <?php require('_footer.php'); ?>
     
 <?php echo $view->placeholder('modal'); ?>
-                            
+
+
+<!-- Reformal -->
+<script type="text/javascript">
+    var reformalOptions = {
+        project_id: 184340,
+        project_host: "vseto.reformal.ru",
+        tab_orientation: "left",
+        tab_indent: "50%",
+        tab_bg_color: "#333333",
+        tab_border_color: "#FFFFFF",
+        tab_image_url: "http://tab.reformal.ru/T9GC0LfRi9Cy0Ysg0Lgg0L%252FRgNC10LTQu9C%252B0LbQtdC90LjRjw==/FFFFFF/2a94cfe6511106e7a48d0af3904e3090/left/1/tab.png",
+        tab_border_width: 0
+    };
+    
+    (function() {
+        var script = document.createElement('script');
+        script.type = 'text/javascript'; script.async = true;
+        script.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'media.reformal.ru/widgets/v3/reformal.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+    })();
+</script><noscript><a href="http://reformal.ru"><img src="http://media.reformal.ru/reformal.png" /></a><a href="http://vseto.reformal.ru">Oтзывы и предложения для vse.to</a></noscript>                            
 </body>     
