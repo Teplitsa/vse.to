c9b015c48edd4a1fdded5e2a3224f45937df3ca8
ivanovser <sivanov@nes.ru>
2013-11-16 11:46:44 +0400

    VK + FB comments (Again)

---
+++ modules/general/comments/config/comments.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	// Ключ api вконтакте
+	"vk_key" 		=> "3999221",
+	// Кол-во отображаемых сообщений вконтакте
+	"vk_messages" 	=> "15",
+	// Ширина виджета вконтакте, в пикселях
+	"vk_width" 		=> "470",
+	
+	// Кол-во отображаемых сообщений facebook
+	"fb_messages" 	=> "15",
+	// Ширина виджета facebook, в пикселях
+	"fb_width" 		=> "470",
+	
+);
