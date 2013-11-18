ed98da43b16c0a721211c18e3541475059f0e023
Alexey Romanov <jgc128@outlook.com>
2013-11-18 15:24:14 +0400

    Update create product form #2

--- modules/shop/catalog/views/frontend/forms/product.php
+++ modules/shop/catalog/views/frontend/forms/product.php
@@ -82,7 +82,7 @@
     </fieldset>
     <?php echo $form->get_element('choalg')->render_alone_errors();?>            
     
-    <div class="b-txt"><label for="">Требования к площадке</label><?php echo $form->get_element('require')->render_input();?></div>
+    <div class="b-txt b-require"><label for="">Требования к площадке</label><?php echo $form->get_element('require')->render_input();?></div>
     <?php echo $form->get_element('require')->render_alone_errors();?>    
 
     <div class="form-action">
