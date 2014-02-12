1b225dcbbfef295c9540f5095fe7d62b25e46512
Alexey Romanov <jgc128@outlook.com>
2014-02-12 14:24:54 +0400

    hangouts button to HOA

--- modules/shop/catalog/views/frontend/products/administrator_nav.php
+++ modules/shop/catalog/views/frontend/products/administrator_nav.php
@@ -1,6 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 $provider = $product->get_telemost_provider();
+$isButton = false;
 
 if ($provider == Model_Product::COMDI) {
     $connectUrl = URL::to('frontend/catalog/product/fullscreen', array('alias' => $product->alias));
@@ -11,7 +12,8 @@ if ($provider == Model_Product::COMDI) {
     if (!empty($product->hangouts_url)) {
         $connectUrl = $product->hangouts_url;
     } else {
-         $connectUrl = "https://plus.google.com/hangouts/_?gid=1085528649580&gd=".$product->hangouts_secret_key;
+        $isButton = true;
+        $connectUrl = '';
     }
     
     $endUrl = URL::site($product->uri_frontend(NULL,Model_Product::STOP_STAGE));
@@ -29,8 +31,24 @@ if ($provider == Model_Product::COMDI) {
             break;                        
         case Model_Product::START_STAGE:
 ?>
-                    <a href="<?php echo $connectUrl; ?>" class="go-link button" target="_blank">Подключиться</a>
-                    <a href="<?php echo $endUrl ?>" class="request-link button">Закончить</a>
+    <?php if($isButton): ?>
+        <div id="hangouts-button"></div>
+        <script src="https://apis.google.com/js/platform.js"></script>
+        <script>
+            
+(function() {
+    gapi.hangout.render('hangouts-button', {
+            'render': 'createhangout',
+            'initial_apps': [{'app_id' : '1085528649580', 'start_data' : '<?php echo $product->hangouts_secret_key;  ?>', 'app_type' : 'ROOM_APP' }],
+            'hangout_type': 'onair',
+            'widget_size': 175 // 136, 72
+    });
+})();
+        </script>    
+    <?php else: ?>
+        <a href="<?php echo $connectUrl; ?>" class="go-link button" target="_blank">Подключиться</a>
+    <?php endif; ?>
+                    <a href="<?php echo $endUrl ?>" style="vertical-align: top" class="request-link button">Закончить</a>
 <?php
             break;
     }
