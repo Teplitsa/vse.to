bfe60057ceee92c68fc37cd13a6a4e27ddf225f4
Alexey Romanov <jgc128@outlook.com>
2014-02-14 13:47:54 +0400

    Close #23

--- modules/shop/area/views/frontend/places/map_place.php
+++ modules/shop/area/views/frontend/places/map_place.php
@@ -1,18 +1,4 @@
 <?php 
 $url = URL::to('frontend/area/places', array('alias' => $place->alias));
-
-$image = $place->image(4);
-
-if (isset($image['image']))
-{
-    $image =  
-            '<a href="#">'
-  .             HTML::image('public/data/' . $image['image'], array('alt' => $place->name))
-  .         '</a>';
-    
-} else {
-    $image ='';
-} 
-?>   
-    <div class="row-fluid map_info"><div class="span3 bio"><?php echo $image ?><?php if (is_array($place->links)) { ?>
-<?php } ?></div><div class="span6 content"><a href="#"><?php echo $place->name?></a><p class="place"><?php echo $place->town_name?>, <?php echo $place->address ?></p><?php echo $place->description ?></div></div>
+?>
+<div class="map_info"><h3><?php echo $place->name?></h3><p class="place"><?php echo $place->town_name?>, <?php echo $place->address ?></p></div>
